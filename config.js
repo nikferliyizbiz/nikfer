cfe0b85ce3b6f3fd84ce5cbf940464858aa83e38
nikferliyizbiz <nikferliyizbiz@gmail.com>
2026-05-29 12:13:43 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 // Supabase Dashboard > Project Settings > API
 // ============================================================
 
-const SUPABASE_URL = 'https://zvspjcuwvvehpwmxhfrp.supabase.co';  // ← değiştirin
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp2c3BqY3V3dnZlaHB3bXhoZnJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk3NDkyNjIsImV4cCI6MjA5NTMyNTI2Mn0.DJurxVEfsUOqSf2MXzQAw-J5hLGT2wxRRjxOl9a2NGM';          // ← değiştirin
+const SUPABASE_URL = 'https://nxifujeuebswxlgwcmbm.supabase.co';  // ← değiştirin
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im54aWZ1amV1ZWJzd3hsZ3djbWJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAwNDIwMjUsImV4cCI6MjA5NTYxODAyNX0.9K9MTu7nv0v2adBVTs3nkSHt0TZgeDpWnWt__Y4h_lY';          // ← değiştirin
 
 export { SUPABASE_URL, SUPABASE_ANON_KEY };
